23a42cc9de238569636bc7814498a876c234931c
Srija-Ghosh <srijaghosh2004@gmail.com>
2026-03-10 17:49:35 +0530

    feat: Made several changes, please update system.

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,7 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import DashboardLayout from "./layouts/DashboardLayout"
 import Dashboard from "./pages/Dashboard"
+import Assessments from "./pages/Assessments"
 import Assessment from "./pages/Assessment"
+import Intelligence from "./pages/Intelligence"
+import Settings from "./pages/Settings"
 
 function App() {
   return (
@@ -9,7 +12,12 @@ function App() {
       <Routes>
         <Route path="/" element={<DashboardLayout />}>
           <Route index element={<Dashboard />} />
-          <Route path="assessment" element={<Assessment />} />
+          <Route path="assessments">
+            <Route index element={<Assessments />} />
+            <Route path="new" element={<Assessment />} />
+          </Route>
+          <Route path="intelligence" element={<Intelligence />} />
+          <Route path="settings" element={<Settings />} />
         </Route>
       </Routes>
     </Router>
